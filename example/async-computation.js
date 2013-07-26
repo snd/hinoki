8c381b776d9eb3c9416f43f0feea7a281185de47
Maximilian Krüger <kruemaxi@googlemail.com>
2013-07-26 02:15:12 +0200

    examples

---
+++ example/async-computation.js
@@ -0,0 +1,26 @@
+var dns = require('dns');
+
+var q = require('q');
+var hinoki = require('../src/hinoki');
+
+var factories = {
+    addresses: function(domain) {
+        return q.nfcall(dns.resolve4, domain);
+    },
+    domains: function(addresses) {
+        return q.all(addresses.map(function(address) {
+            return q.nfcall(dns.reverse, address);
+        }));
+    }
+};
+
+var container = {
+    factories: factories,
+    instances: {
+        domain: 'www.google.com'
+    }
+};
+
+hinoki.inject(container, function(domains) {
+    console.log(domains);
+});
---
+++ example/computation.js
@@ -0,0 +1,33 @@
+var hinoki = require('../src/hinoki');
+
+var factories = {
+    count: function(xs) {
+        return xs.length;
+    },
+    mean: function(xs, count) {
+        var reducer = function(acc, x) {
+            return acc + x;
+        };
+        return xs.reduce(reducer, 0) / count;
+    },
+    meanOfSquares: function(xs, count) {
+        var reducer = function(acc, x) {
+            return acc + x * x;
+        };
+        return xs.reduce(reducer, 0) / count;
+    },
+    variance: function(mean, meanOfSquares) {
+        return meanOfSquares - mean * mean;
+    }
+};
+
+var container = {
+    factories: factories,
+    instances: {
+        xs: [1, 2, 3, 6]
+    }
+};
+
+hinoki.inject(container, function(mean) {
+    console.log(mean);  // -> 3
+});
