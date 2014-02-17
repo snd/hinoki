22cc55a69fce292705b14aef863344d210960d9b
Maximilian Krüger <kruemaxi@googlemail.com>
2014-02-17 17:22:47 +0100

    update examples

---
+++ example/async-bluebird.js
@@ -0,0 +1,36 @@
+var events = require('events');
+
+var Promise = require('bluebird');
+
+var dns = Promise.promisifyAll(require('dns'));
+
+var hinoki = require('hinoki');
+
+var emitter = new events.EventEmitter();
+
+emitter.on('any', function(event, a, b) {
+    console.log('ANY', event, a, b);
+});
+
+var factories = {
+    addresses: function(domain) {
+        return dns.resolve4Async(domain);
+    },
+    domains: function(addresses) {
+        return Promise.map(addresses, function(address) {
+            return dns.reverseAsync(address);
+        });
+    }
+};
+
+var container = {
+    factories: factories,
+    instances: {
+        domain: 'www.google.com'
+    },
+    emitter: emitter
+};
+
+hinoki.inject(container, function(domains) {
+    console.log(domains);
+});
