52f78ce2e454a8531521a0bdf7320d0de8d6a355
Maximilian Krüger <kruemaxi@googlemail.com>
2014-07-26 22:20:18 +0200

    add example

---
+++ example/dollar-inject.js
@@ -0,0 +1,22 @@
+var hinoki = require('hinoki');
+
+var factories = {
+  a: function() { return 'a'; },
+  b: function() { return 'b'; },
+  c: function() { return 'c'; },
+  d: function() { return 'd'; },
+  // this should depend on ['a', 'c'], we override this below
+  ac: function(a, b) { return a + b; },
+  acd: function(ac, d) { return ac + d; }
+};
+
+factories.ac.$inject = ['a', 'c'];
+
+var container = hinoki.newContainer(factories);
+
+hinoki.get(container, 'acd', console.log).then(function(acd) {
+  console.log(acd);  // -> 'acd'
+  // dependency ids have been cached
+  console.log(factories.a.$inject); // -> []
+  console.log(factories.acd.$inject); // -> ['ac', 'd']
+});
